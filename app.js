cc5820c0927de89b4fd7e4c0f07953d63e464c47
Tim McNamara <timmcnamara1990@Tims-MacBook-Pro.local>
2017-08-14 19:59:41 -0400

    added socket, translate packages, and socket connection to backend

--- app.js
+++ app.js
@@ -9,16 +9,47 @@ const session= require("express-session");
 const Query = require('./Query');
 const mysql = require('promise-mysql');
 const cors = require('cors');
+const socket = require('socket.io');
+// temp translate API
+const translate = require('google-translate-api');
+const io = socket(server);
 //
 
-//basic setup for express server
-//https://www.tutorialspoint.com/expressjs/expressjs_hello_world.htm
+var server = app.listen(3000, function(){
+    console.log('listening for requests on port 3000,');
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        console.log(data)
+        translate(data.message, {to: 'fr'})
+        .then( trans => {
+          data.message = trans.text;
+          console.log(data, "this is the data")
+          io.sockets.emit('chat', data);
+        }
+        )
+        console.log(data)
+        // console.log(data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
 
 // parse incoming requests
 app.use(bodyParser.json());
 
 
- // create a connection to the DB
+// create a connection to the DB
    const connection = mysql.createPool({
        host     : 'localhost',
        user     : 'root',
@@ -69,13 +100,12 @@ app.post('/signup', function(req, res){
   });
 });
 
-app.get(
-  '/login', function (req, res){
+
+app.get('/login', function (req, res){
   res.send("login page")
 })
+
 
 app.post('/login', function (req,res){
   res.send("post login!");
 })
-
-app.listen(3000,console.log("listen to port 3000"));
