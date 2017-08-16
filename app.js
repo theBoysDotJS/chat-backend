027fadc383bf7f7258d4052410e747fdc6cf0116
Mike McDonald <mikemcd97@gmail.com>
2017-08-16 12:04:56 -0400

    got rid of cors and manually set headers to fix compatability problem with windows

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 
 //import express library
 const express = require("express");
+const app = express();
 const bodyParser= require("body-parser");
 const morgan= require("morgan");
 const session= require("express-session");
 const mysql = require('promise-mysql');
-const cors = require('cors');
-const socket = require('socket.io');
+// const cors = require('cors');
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 // temp translate API
 const translate = require('google-translate-api');
-const io = socket(server);
+// const io = socket(server);
 const checkLoginToken = require('./lib/check-login-token.js');
 // Create new express web server
-const app = express();
+
+
 
 
 // Data Loader
@@ -37,14 +40,23 @@ const messageController = require('./controllers/message.js');
 //Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 //app.use(checkLoginToken(queryAPI));
 app.use('/auth', authController(queryAPI));
+app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "http://localhost:3001");
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    }
+);
 
 
 
-var server = app.listen(3000, function(){
-    console.log('listening for requests on port 3000,');
+ http.listen(8080, function(){
+    console.log('listening for requests on port 8080,');
 });
 
 // Socket.io logic
@@ -60,15 +72,15 @@ io.on('connection', (socket) => {
         //
 
         // if statements to filter the data.
-        console.log(data)
-        translate(data.message, {to: 'fr'})
+        console.log(data, "this is the new log")
+        translate(data.text, {to: 'fr'})
         .then( trans => {
-          data.message = trans.text;
+          data.text = trans.text;
           console.log(data, "this is the data")
           io.sockets.emit('chat', data);
         }
         )
-        console.log(data)
+        console.log(data, "look at me")
         // console.log(data);
 
     });
