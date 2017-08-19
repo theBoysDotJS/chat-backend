0117cbb76bc4cab5b90128c50bf5f8f074b61273
Quinn Langille <quinn@quinnlangille.com>
2017-08-19 18:01:00 -0400

    testing change in cors headers

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 
 	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://theboyschatapp.herokuapp.com');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
