a0a7729bd02ce1072bd2153c12acc1aeec61f366
Quinn Langille <quinn@quinnlangille.com>
2017-08-19 17:56:38 -0400

    added heroku env port to app.listen

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 //import express library
 const express = require("express");
 const app = express();
-const bodyParser= require("body-parser");
-const morgan= require("morgan");
-const session= require("express-session");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("express-session");
 const mysql = require('promise-mysql');
 const cors = require('cors');
 
@@ -18,17 +18,11 @@ const checkLoginToken = require('./lib/check-login-token.js');
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
 
-
 // Data Loader
 const Query = require('./lib/Query');
 
 // Create a connection to the DB
-const connection = mysql.createPool({
-   host     : 'us-cdbr-iron-east-05.cleardb.net',
-   user     : 'b537a8dc95ca1e',
-   password : '6b5c43b1',
-   database : 'heroku_fd5680f97c93408'
-});
+const connection = mysql.createPool({host: 'us-cdbr-iron-east-05.cleardb.net', user: 'b537a8dc95ca1e', password: '6b5c43b1', database: 'heroku_fd5680f97c93408'});
 
 const queryAPI = new Query(connection);
 
@@ -37,30 +31,29 @@ const authController = require('./controllers/auth.js');
 const conversationController = require('./controllers/conversation.js');
 const messageController = require('./controllers/message.js');
 
-
 //Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 // app.use(cors());
 
 // Add headers
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://theboyschatapp.herokuapp.com');
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', 'https://theboyschatapp.herokuapp.com');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+	// Pass to next layer of middleware
+	next();
 });
 
 app.use(checkLoginToken(queryAPI));
@@ -68,52 +61,45 @@ app.use('/auth', authController(queryAPI));
 app.use('/message', messageController(queryAPI));
 app.use('/conversation', conversationController(queryAPI));
 
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 })
 
-
 let port = 3001;
-http.listen(port, function(){
-   console.log(`listening for requests on port ${port}`);
+http.listen(process.env.PORT || port, function() {
+	console.log(`listening for requests on port ${port}`);
 });
 
 // Socket.io logic
 
 io.on('connection', (socket) => {
 
-  console.log('made socket connection', socket.id);
-  // Handle chat event
-  socket.on('chat', function(data){
+	console.log('made socket connection', socket.id);
+	// Handle chat event
+	socket.on('chat', function(data) {
 
+		queryAPI.messageReceived(data).then(result => {
+			data["messageId"] = result.insertId
 
-    queryAPI.messageReceived(data)
-    .then(result => {
-      data["messageId"]=result.insertId
+			queryAPI.getUserLanguage(data.user).then(rowData => {
 
-      queryAPI.getUserLanguage(data.user)
-      .then(rowData=> {
+				translate(data.text, {to: rowData[0].language}).then(trans => {
+					data.text = trans.text;
 
-        translate(data.text, {to:
-          rowData[0].language})
-        .then( trans => {
-          data.text = trans.text;
+					//add user.id in my text
 
-          //add user.id in my text
+					//kind of a return
 
-          //kind of a return
+					io.sockets.emit('chat', data);
+				})
+			})
 
-          io.sockets.emit('chat', data);
-        })
-      })
+		})
+	});
 
-    })
-  });
-
-  // Handle typing event
-  // socket.on('typing', function(data){
-  //     socket.broadcast.emit('typing', data);
-  // });
-
+	// Handle typing event
+	// socket.on('typing', function(data){
+	//     socket.broadcast.emit('typing', data);
+	// });
 
 });
